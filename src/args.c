c07836602dd4cdf4dc9466e425a2d46740ce905d
Philippe Caron <philippe-caron@hotmail.com>
2024-06-20 11:48:13 -0400

    Works for now (needs more testing)

--- src/args.c
+++ src/args.c
@@ -40,6 +40,104 @@ int _param_start()
 }
 
 /******************************************************************************/
+ArgOption *_option(const char ident)
+{
+  ArgOption *found = NULL;
+
+  int n_options = _param_start();
+  int n_default = sizeof(_DEFAULT_OPTIONS) / sizeof(ArgOption);
+
+  for (int i = 0; i < n_options; i++) {
+    if (_OPTIONS[i].ident == ident) {
+      found = &_OPTIONS[i];
+      break;
+    }
+  }
+
+  if (!found) {
+    for (int i = 0; i < n_default; i++) {
+      if (_DEFAULT_OPTIONS[i].ident == ident) {
+        found = &_DEFAULT_OPTIONS[i];
+        break;
+      }
+    }
+  }
+
+  return found;
+}
+
+/******************************************************************************/
+ArgOption *_loption(const char *name)
+{
+  ArgOption *found = NULL;
+
+  int n_options = _param_start();
+  int n_default = sizeof(_DEFAULT_OPTIONS) / sizeof(ArgOption);
+
+  for (int i = 0; i < n_options; i++) {
+    if (_OPTIONS[i].name && !strcmp(_OPTIONS[i].name, name)) {
+      found = &_OPTIONS[i];
+      break;
+    }
+  }
+
+  if (!found) {
+    for (int i = 0; i < n_default; i++) {
+      if (!strcmp(_DEFAULT_OPTIONS[i].name, name)) {
+        found = &_DEFAULT_OPTIONS[i];
+        break;
+      }
+    }
+  }
+
+  return found;
+}
+
+/******************************************************************************/
+ArgOption *_parameter(const char *name)
+{
+  ArgOption *found = NULL;
+
+  for (int i = _param_start(); _OPTIONS[i].ident; i++) {
+    if (!strcmp(_OPTIONS[i].name, name)) {
+        found = &_OPTIONS[i];
+        break;
+      }
+  }
+
+  return found;
+}
+
+/******************************************************************************/
+ArgValue _parse_option(ArgOption *option, const char *parameter)
+{
+  ArgValue value;
+
+  switch (option->type) {
+    case ARG_TYPE_ANY:
+    // Casting is performed by used, so treat as charptr
+    case ARG_TYPE_CHARPTR:
+      value.as_charptr = parameter;
+      break;
+    case ARG_TYPE_INTEGER:
+      value.as_integer = atoi(parameter);
+      break;
+    case ARG_TYPE_DECIMAL:
+      value.as_decimal = atof(parameter);
+      break;
+    case ARG_TYPE_BOOLEAN:
+      value.as_integer = !strcmp("true", parameter);
+      break;
+    case ARG_TYPE_NONE:
+    default:
+      value.as_charptr = NULL;
+      break;
+  }
+
+  return value;
+}
+
+/******************************************************************************/
 char *_expects(ArgOption *option)
 {
   char *expects;
@@ -211,100 +309,9 @@ void _default_version(Args *args, ArgValue value)
 }
 
 /******************************************************************************/
-ArgValue _parse_option(ArgOption *option, const char *parameter)
-{
-  ArgValue value;
-
-  switch (option->type) {
-    case ARG_TYPE_ANY:
-    // Casting is performed by used, so treat as charptr
-    case ARG_TYPE_CHARPTR:
-      value.as_charptr = parameter;
-      break;
-    case ARG_TYPE_INTEGER:
-      value.as_integer = atoi(parameter);
-      break;
-    case ARG_TYPE_DECIMAL:
-      value.as_decimal = atof(parameter);
-      break;
-    case ARG_TYPE_BOOLEAN:
-      value.as_integer = !strcmp("true", parameter);
-      break;
-    case ARG_TYPE_NONE:
-    default:
-      value.as_charptr = NULL;
-      break;
-  }
-
-  return value;
-}
-
-/******************************************************************************/
-void _(parameter)(int* index, const char *value)
-{
-  ArgOption *parameter = &_OPTIONS[*index];
-
-  if (parameter->ident != ' ' && parameter->ident != '*') (*index)++;
-
-  Map_setkey(_this->parameters, NEW (String) (parameter->name), NEW (String) (value));
-}
-
-/******************************************************************************/
-ArgOption* _(option)(char ident)
-{
-  ArgOption *found = NULL;
-
-  int n_default = sizeof(_DEFAULT_OPTIONS) / sizeof(ArgOption);
-
-  for (int i = 0; _OPTIONS[i].ident; i++) {
-    if (_OPTIONS[i].ident == ident) {
-      found = &_OPTIONS[i];
-      break;
-    }
-  }
-
-  if (!found) {
-    for (int i = 0; i < n_default; i++) {
-      if (_DEFAULT_OPTIONS[i].ident == ident) {
-        found = &_DEFAULT_OPTIONS[i];
-        break;
-      }
-    }
-  }
-
-  return found;
-}
-
-/******************************************************************************/
 void _(optcall)(ArgOption *option, const char *param)
 {
-  option->callback(_this, _parse_option(option, param));
-}
-
-/******************************************************************************/
-ArgOption* _(loption)(const char *name)
-{
-  ArgOption *found = NULL;
-
-  int n_default = sizeof(_DEFAULT_OPTIONS) / sizeof(ArgOption);
-
-  for (int i = 0; _OPTIONS[i].ident; i++) {
-    if (!strcmp(_OPTIONS[i].name, name)) {
-      found = &_OPTIONS[i];
-      break;
-    }
-  }
-
-  if (!found) {
-    for (int i = 0; i < n_default; i++) {
-      if (!strcmp(_DEFAULT_OPTIONS[i].name, name)) {
-        found = &_DEFAULT_OPTIONS[i];
-        break;
-      }
-    }
-  }
-
-  return found;
+  if (option->callback) option->callback(_this, _parse_option(option, param));
 }
 
 /******************************************************************************/
@@ -318,14 +325,35 @@ void _(loptcall)(const char *option)
   // Separate the name and the value around the "=" sign
   if (param) {
     name = buffer;
-    memcpy(buffer, option, ++param - option);
+    memcpy(buffer, option, param++ - option);
   }
 
   {
-    ArgOption *opt = Args_loption(_this, name);
+    ArgOption *opt = _loption(name);
 
-    if (opt) Args_optcall(_this, opt, param);
+    if (opt) {
+      if (opt->type != ARG_TYPE_NONE && !param) {
+        fprintf(stderr, "Parameter is missing for option '%s'.\n", opt->name);
+        exit(0);
+      } else {
+        Args_optcall(_this, opt, param);
+      }
+    } else {
+      fprintf(stderr, "Unreckognized option '%s'.\n", option);
+      exit(0);
+    }
   }
+}
+
+/******************************************************************************/
+void _(pcall)(int* index, const char *value)
+{
+  ArgOption *parameter = &_OPTIONS[*index];
+
+  if (parameter->ident != ' ' && parameter->ident != '*') (*index)++;
+
+  Map_setkey(_this->parameters, NEW (String) (parameter->name), NEW (String) (value));
+  Args_optcall(_this, parameter, value);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -343,7 +371,7 @@ TYPENAME *_(cons)(int argc, char *argv[], void *env)
     _this->program_major = _VERSION_MAJOR;
     _this->program_minor = _VERSION_MINOR;
     _this->program_name  = NEW (String) (buffer);
-    _this->parameters    = NEW (Map)    (sizeof(String), sizeof(String), (Comparer)String_equals);
+    _this->parameters    = NEW (Map)    (sizeof(String), sizeof(String), (Comparer)String_cequals);
 
     for (int i = 1; i < argc; i++) {
       char *arg = argv[i];
@@ -353,7 +381,7 @@ TYPENAME *_(cons)(int argc, char *argv[], void *env)
 
       // TODO: Remove param mode
       if (param_mode || arg[0] != '-') {        
-        Args_parameter(_this, &param_start, arg);
+        Args_pcall(_this, &param_start, arg);
       }
       else if (arg[1] == '-') {
         if (!arg[2]) {
@@ -366,14 +394,18 @@ TYPENAME *_(cons)(int argc, char *argv[], void *env)
       } else {
         // Normal option
         for (int j = 1; arg[j]; j++) {
-          ArgOption* option = Args_option(_this, arg[j]);
+          ArgOption* option = _option(arg[j]);
 
           if (option) {
             if (option->type == ARG_TYPE_NONE) {
               Args_optcall(_this, option, NULL);
             } else if (!arg[j + 1]) {
               // Split param option
-              Args_optcall(_this, option, argv[++i]);
+              if (++i < argc) Args_optcall(_this, option, argv[i]);
+              else {
+                fprintf(stderr, "Parameter is missing for option '%c'.\n", arg[j]);
+                exit(0);
+              }
               break;
             } else {
               // Contiguous param option
@@ -382,7 +414,6 @@ TYPENAME *_(cons)(int argc, char *argv[], void *env)
             }
           } else {
             fprintf(stderr, "Unreckognized option '%c'.\n", arg[j]);
-            
             exit(0);
           }
         }
@@ -403,36 +434,56 @@ void _(free)()
 ////////////////////////////////////////////////////////////////////////////////
 ArgValue _(index)(int index)
 {
-  Pair      *pair   = Array_at((Array*)_this->parameters, index);
-  ArgOption *option = Args_loption(_this, *(char**)pair->first);
+  ArgValue  value = { .as_charptr = NULL };
+  Pair     *pair  = Array_at((Array*)_this->parameters, index);
 
-  return _parse_option(option, *(char**)pair->second);
+  if (pair) {
+    ArgOption *param = _parameter(*(char**)pair->first);
+
+    if (param) value = _parse_option(param, *(char**)pair->second);
+  }
+
+  return value;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 ArgValue _(name)(const char* name)
 {
-  Pair      *pair   = Map_atkey(_this->parameters, (void*)name);
-  ArgOption *option = Args_loption(_this, *(char**)pair->first);
+  ArgValue  value = { .as_charptr = NULL };
+  Pair     *pair  = Map_atkey(_this->parameters, name);
 
-  return _parse_option(option, *(char**)pair->second);
+  if (pair) {
+    ArgOption *param = _parameter(*(char**)pair->first);
+
+    if (param) value = _parse_option(param, *(char**)pair->second);
+  }
+
+  return value;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 Array* _(list)()
 {
-  ArgOption *option = Args_option(_this, '*');
-  Array     *params = (Array*)_this->parameters;
-  Array     *list   = NEW (Array) (sizeof(ArgValue));
+  Array *list      = NULL;
+  int    n_options = _sizeof_options();
 
+  if (n_options > 0) {
+    ArgOption *option = &_OPTIONS[n_options - 1];
 
-  for (int i = 0; i < params->size; i++) {
-    Pair *pair = (Pair*)Array_at(params, i);
+    if (option->ident == '*') {
+      Array *params = (Array*)_this->parameters;
+      
+      list = NEW (Array) (sizeof(ArgValue));
 
-    if (!strcmp(option->name, *(char**)pair->first)) {
-      ArgValue value = _parse_option(option, *(char**)pair->second);
+      for (int i = 0; i < params->size; i++) {
+        Pair *pair = (Pair*)Array_at(params, i);
 
-      Array_push(list, &value);
+        if (!strcmp(option->name, *(char**)pair->first)) {
+          ArgValue value = _parse_option(option, *(char**)pair->second);
+
+          Array_push(list, &value);
+        }
+      }
     }
   }
 
--- tst/main.c
+++ tst/main.c
@@ -1,22 +1,50 @@
 #include <diagnostic.h>
 #include <args.h>
 
+typedef struct _env {
+  int         num;
+  const char *name;
+} Env;
 
-//gcc tst/test.c src/args.c -Iinc -I../Collection/inc -I../Diagnostic/inc -I../OOP/inc -I../IO/inc -I../String/inc -I../Stream/inc -Llib -L../String/lib -lstring0 -L../OOP/lib -loop0 -L../Stream/lib -lstream0 -L../Collection/lib -lcollection0 -Llib -largs0
+void setnum(Args *args, ArgValue value)
+{
+  Env *env = args->env;
 
-//VERSION(0, 0);
+  env->num = value.as_integer;
+}
+
+void setname(Args *args, ArgValue value)
+{
+  Env *env = args->env;
+
+  env->name = value.as_charptr;
+}
+
 OPTIONS(
-  { "option", 'o', "Specify an option (between 1 and 10)", ARG_TYPE_INTEGER, NULL },
-  { "name"  , 'n', "Does the program have a name?"       , ARG_TYPE_CHARPTR, NULL },
-  { "test"  , '+', "If name, specify here"               , ARG_TYPE_CHARPTR, NULL },
-  { ""      , '-', "Optional"                            , ARG_TYPE_ANY,     NULL }
+  { "option", 'o', "Specify an option (between 1 and 10)", ARG_TYPE_INTEGER, setnum  },
+  { "name"  , 'n', "Does the program have a name?"       , ARG_TYPE_CHARPTR, setname },
+  { "test"  , '+', "If name, specify here"               , ARG_TYPE_CHARPTR, NULL    },
+  { ""      , '-', "Optional"                            , ARG_TYPE_ANY,     NULL    }
 );
 
 int main(int argc, char *argv[])
 {
-  Args *args = NEW (Args) (argc, argv, NULL);
+  Env   env = { 0, "" };
+  Args *args = NEW (Args) (argc, argv, &env);
 
   CHECK_MEMORY
+
+  if (env.num)
+  {
+    printf("Num: %d\n", env.num);
+  }
+
+  if (env.name[0])
+  {
+    printf("Name: %s\n", env.name);
+  }
+
+  printf("Test: %s\n", Args_index(args, 0).as_charptr);
 
   DELETE (args);
 
